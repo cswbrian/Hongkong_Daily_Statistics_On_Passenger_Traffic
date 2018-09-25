a29a963e5a8f45e6559ae1ec2ec4ff899ed4fce4
coolsunwind <brianleung1017@gmail.com>
2018-09-26 01:13:03 +0800

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-import scraperwiki
+# import scraperwiki
 
 import requests
 from bs4 import BeautifulSoup
@@ -18,21 +18,23 @@ ytdYmd = ytd.strftime("%Y%m%d")
 # Scrape Page
 r = requests.get("https://www.immd.gov.hk/eng/stat_{}.html".format(ytdYmd))
 soup = BeautifulSoup(r.text, "html.parser")
-tr = soup.find_all("tr", class_="")
-for row in tr[1:]:
-    td = row.find_all("td")
-    ctrlPt_data = {
-        'date': ytd.strftime("%Y-%m-%d"),
-        'control_point': td[0].text,
-        'arrival_Hong_Kong_Residents': int(td[1].text.replace(',','')),
-        'arrival_Mainland_Visitors': int(td[2].text.replace(',','')),
-        'arrival_Other_Visitors': int(td[3].text.replace(',','')),
-        'arrival_Total': int(td[4].text.replace(',','')),
-        'departure_Hong_Kong_Residents': int(td[5].text.replace(',','')),
-        'departure_Mainland_Visitors': int(td[6].text.replace(',','')),
-        'departure_Other_Visitors': int(td[7].text.replace(',','')),
-        'departure_Total': int(td[8].text.replace(',',''))
-    }
-    scraperwiki.sqlite.save(unique_keys=['date', 'control_point'], data=ctrlPt_data)
-    print(ctrlPt_data)
+tr = soup.find_all("tr")
+for row in tr:
+    cp = row.find("td", class_ ="")
+    if cp:
+        td = row.find_all("td", class_ ="hRight")
+        ctrlPt_data = {
+            'date': ytd.strftime("%Y-%m-%d"),
+            'control_point': cp.text,
+            'arrival_Hong_Kong_Residents': int(td[0].text.replace(',','')),
+            'arrival_Mainland_Visitors': int(td[1].text.replace(',','')),
+            'arrival_Other_Visitors': int(td[2].text.replace(',','')),
+            'arrival_Total': int(td[3].text.replace(',','')),
+            'departure_Hong_Kong_Residents': int(td[4].text.replace(',','')),
+            'departure_Mainland_Visitors': int(td[5].text.replace(',','')),
+            'departure_Other_Visitors': int(td[6].text.replace(',','')),
+            'departure_Total': int(td[7].text.replace(',',''))
+        }
+        scraperwiki.sqlite.save(unique_keys=['date', 'control_point'], data=ctrlPt_data)
+        print(ctrlPt_data)
 print(ytdYmd)
