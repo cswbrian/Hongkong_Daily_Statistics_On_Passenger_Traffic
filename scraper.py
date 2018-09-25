ebb0031a41c5a53bc4fe81624bfc22f67c474e33
coolsunwind <brianleung1017@gmail.com>
2018-09-26 00:42:04 +0800

    add scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,38 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import requests
+from bs4 import BeautifulSoup
+import re
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+from datetime import datetime
+from datetime import timedelta
+import pytz
+
+
+# Get Date
+hkt = pytz.timezone('Asia/Hong_Kong')
+ytd = datetime.now().replace(tzinfo=hkt)-timedelta(days=2)
+ytdYmd = ytd.strftime("%Y%m%d")
+
+
+# Scrape Page
+r = requests.get("https://www.immd.gov.hk/eng/stat_{}.html".format(ytdYmd))
+soup = BeautifulSoup(r.text, "html.parser")
+tr = soup.find_all("tr", class_="")
+for row in tr[1:]:
+    td = row.find_all("td")
+    ctrlPt_data = {
+        'date_control_point': "{}-{}".format(ytd.strftime("%Y-%m-%d"),td[0].text),
+        'arrival_Hong_Kong_Residents': int(td[1].text.replace(',','')),
+        'arrival_Mainland_Visitors': int(td[2].text.replace(',','')),
+        'arrival_Other_Visitors': int(td[3].text.replace(',','')),
+        'arrival_Total': int(td[4].text.replace(',','')),
+        'departure_Hong_Kong_Residents': int(td[5].text.replace(',','')),
+        'departure_Mainland_Visitors': int(td[6].text.replace(',','')),
+        'departure_Other_Visitors': int(td[7].text.replace(',','')),
+        'departure_Total': int(td[8].text.replace(',',''))
+    }
+    print(ctrlPt_data)
+print(ytdYmd)
+scraperwiki.sqlite.save(unique_keys=['date_control_point'], data=ctrlPt_data)
