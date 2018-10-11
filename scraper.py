20795af0e59202af85c80fb7d9d49305f83fc0ea
coolsunwind <brianleung1017@gmail.com>
2018-10-11 23:42:18 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,11 +9,14 @@ from datetime import timedelta
 import pytz
 
 
+date = "20181005"
+date1 = "2018-10-05"
+
 # Get Date
 twoDayAgo = datetime.now() + timedelta(hours=8)-timedelta(days=2)
 
 # Scrape Page
-r = requests.get("https://www.immd.gov.hk/eng/stat_{}.html".format(twoDayAgo.strftime("%Y%m%d")))
+r = requests.get("https://www.immd.gov.hk/eng/stat_{}.html".format(date))
 soup = BeautifulSoup(r.text, "html.parser")
 tr = soup.find_all("tr")
 for row in tr:
@@ -21,7 +24,7 @@ for row in tr:
     if cp:
         td = row.find_all("td", class_ ="hRight")
         ctrlPt_data = {
-            'date': twoDayAgo.strftime("%Y-%m-%d"),
+            'date': date1,
             'control_point': cp.text,
             'arrival_Hong_Kong_Residents': int(td[0].text.replace(',','')),
             'arrival_Mainland_Visitors': int(td[1].text.replace(',','')),
@@ -35,4 +38,4 @@ for row in tr:
         scraperwiki.sqlite.save(unique_keys=['date', 'control_point'], data=ctrlPt_data)
         print(ctrlPt_data)
 
-print(twoDayAgo.strftime("%Y-%m-%d"))
+print(date1)
